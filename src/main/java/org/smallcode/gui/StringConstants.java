dd2f58a7706e24560c5c9d5797d17cb997317a9f
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2014-12-17 13:08:30 +0545

    Update StringConstants.java

--- src/main/java/org/smallcode/gui/StringConstants.java
+++ src/main/java/org/smallcode/gui/StringConstants.java
@@ -1,12 +1,9 @@
 package org.smallcode.gui;
 
 /**
- * Created with IntelliJ IDEA.
- * User: deerwalk
- * Date: 9/12/13
- * Time: 10:02 AM
- * To change this template use File | Settings | File Templates.
- */
+* @author Hikmat Dhamee
+* @email me.hemant.available@gmail.com
+*/
 public class StringConstants {
   public static final   String[] recordTypes=new String[]{"Eligibility","Medical","Pharmacy","Hra","Biometric","Lab"};
   public static final   String derivedRecordTypes="MemberSearch;memberCount,ParticipationPTPN;participationPTPNCount";
